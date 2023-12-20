576348f55b954a0da641c09a0161ff93986b8e6f
jackdisalvatore <jackp.disalvatore@gmail.com>
2023-12-20 13:18:51 -0600

    removing unnessacary store subscribtion
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/groups/details/+layout.js
+++ frontend/src/routes/groups/details/+layout.js
@@ -1,5 +1,3 @@
-import groupDetailsButton from '../../../stores/groupDetailsButton';
-
 export function load() {
 	return {
 		menuOptions: [
@@ -7,13 +5,6 @@ export function load() {
 			{ id: 2, label: 'Topics', active: false },
 			{ id: 3, label: 'Applications', active: false },
 			{ id: 4, label: 'Grants', active: false }
-		],
-		groupButton: new Promise(resolve => {
-			const unsubscribe = groupDetailsButton.subscribe(value => {
-				resolve({
-					value
-				});
-			});
-		})
+		]
 	};
 }
