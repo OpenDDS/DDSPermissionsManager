3e66a8828c47ffeee2c45e5e74c776dd500cc594
jackdisalvatore <jackp.disalvatore@gmail.com>
2023-12-21 09:09:51 -0600

    adding pagination to the users table
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/groups/details/UserTable.svelte
+++ frontend/src/routes/groups/details/UserTable.svelte
@@ -3,12 +3,18 @@
 	import groupsSVG from '../../../icons/groups.svg';
 	import topicsSVG from '../../../icons/topics.svg';
 	import appsSVG from '../../../icons/apps.svg';
+	import pageforwardSVG from '../../../icons/pageforward.svg';
+	import pagebackwardsSVG from '../../../icons/pagebackwards.svg';
+	import pagefirstSVG from '../../../icons/pagefirst.svg';
+	import pagelastSVG from '../../../icons/pagelast.svg';
 	import groupContext from '../../../stores/groupContext';
 	import groupMembershipList from '../../../stores/groupMembershipList';
 	import { httpAdapter } from '../../../appconfig';
 	import groupMembershipsTotalPages from '../../../stores/groupMembershipsTotalPages';
 	import groupMembershipsTotalSize from '../../../stores/groupMembershipsTotalSize';
 	import { afterUpdate, onMount } from 'svelte';
+	import retrievedTimestamps from '../../../stores/retrievedTimestamps.js';
+	import RetrievedTimestamp from './../../../lib/RetrievedTimestamp.svelte';
 
 	// Promises
 	let promise;
@@ -69,6 +75,14 @@
 		groupMembershipsTotalPages.set(totalPages);
 		if (totalSize !== undefined) groupMembershipsTotalSize.set(totalSize);
 		groupMembershipsCurrentPage = 0;
+	};
+
+	const deselectAllGroupMembershipCheckboxes = () => {
+		usersAllRowsSelectedTrue = false;
+		usersRowsSelectedTrue = false;
+		usersRowsSelected = [];
+		let checkboxes = document.querySelectorAll('.group-membership-checkbox');
+		checkboxes.forEach((checkbox) => (checkbox.checked = false));
 	};
 
 	onMount(async () => {
@@ -157,4 +171,97 @@
 		{/each}
 		</tbody>
 	</table>
+
+	{#if $groupMembershipsTotalSize !== undefined && $groupMembershipsTotalSize != NaN}
+		<div class="pagination">
+			<span>{messages['pagination']['rows.per.page']}</span>
+			<select
+				tabindex="-1"
+				on:change={(e) => {
+					groupMembershipsPerPage = e.target.value;
+
+					reloadGroupMemberships();
+				}}
+				name="RowsPerPage"
+			>
+				<option value="10">10</option>
+				<option value="25">25</option>
+				<option value="50">50</option>
+				<option value="75">75</option>
+				<option value="100">100&nbsp;</option>
+			</select>
+			<span style="margin: 0 2rem 0 2rem">
+				{#if $groupMembershipsTotalSize > 0}
+					{1 + groupMembershipsCurrentPage * groupMembershipsPerPage}
+				{:else}
+					0
+				{/if}
+				- {Math.min(
+					groupMembershipsPerPage * (groupMembershipsCurrentPage + 1),
+					$groupMembershipsTotalSize
+				)} of {$groupMembershipsTotalSize}
+			</span>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				src={pagefirstSVG}
+				alt="first page"
+				class="pagination-image"
+				class:disabled-img={groupMembershipsCurrentPage === 0}
+				on:click={() => {
+					deselectAllGroupMembershipCheckboxes();
+					if (groupMembershipsCurrentPage > 0) {
+						groupMembershipsCurrentPage = 0;
+
+						reloadGroupMemberships();
+					}
+				}}
+			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				src={pagebackwardsSVG}
+				alt="previous page"
+				class="pagination-image"
+				class:disabled-img={groupMembershipsCurrentPage === 0}
+				on:click={() => {
+					deselectAllGroupMembershipCheckboxes();
+					if (groupMembershipsCurrentPage > 0) {
+						groupMembershipsCurrentPage--;
+						reloadGroupMemberships(groupMembershipsCurrentPage);
+					}
+				}}
+			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				src={pageforwardSVG}
+				alt="next page"
+				class="pagination-image"
+				class:disabled-img={groupMembershipsCurrentPage + 1 === $groupMembershipsTotalPages ||
+					$groupMembershipList?.length === undefined}
+				on:click={() => {
+					deselectAllGroupMembershipCheckboxes();
+					if (groupMembershipsCurrentPage + 1 < $groupMembershipsTotalPages) {
+						groupMembershipsCurrentPage++;
+						reloadGroupMemberships(groupMembershipsCurrentPage);
+					}
+				}}
+			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				src={pagelastSVG}
+				alt="last page"
+				class="pagination-image"
+				class:disabled-img={groupMembershipsCurrentPage + 1 === $groupMembershipsTotalPages ||
+					$groupMembershipList?.length === undefined}
+				on:click={() => {
+					deselectAllGroupMembershipCheckboxes();
+					if (groupMembershipsCurrentPage < $groupMembershipsTotalPages) {
+						groupMembershipsCurrentPage = $groupMembershipsTotalPages - 1;
+						reloadGroupMemberships(groupMembershipsCurrentPage);
+					}
+				}}
+			/>
+		</div>
+
+		<RetrievedTimestamp retrievedTimestamp={$retrievedTimestamps['users']} />
+	{/if}
 {/if}
